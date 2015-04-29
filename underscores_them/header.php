daa1e94f1febc524a908fe0d0a253b43bb6e679e
Margo Junker <margo.junker@khk.ee>
2015-04-29 14:54:16 +0300

    Päevalõpp 29.04.2015
    Footer on ok, Main page on 50 % ready ja header vajab tuunimist, teised
    lehed puudu.

--- underscores_them/header.php
+++ underscores_them/header.php
@@ -14,14 +14,15 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
-
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap-affix.js"></script>
 
 <?php wp_enqueue_script("jquery"); ?>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<!--<div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'underscores_theme' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
@@ -30,10 +31,45 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+<!--		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'underscores_theme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<!--</header><!-- #masthead -->
+<!-- Header -->
+<header>
+    <nav class="navbar navbar-default wrapper" style="margin-bottom: 0px;">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand"></a>
+        </div>
 
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a class="rssfeed hidden-xs hidden-sm navbar-right" href="#"><img class="img-responsive rssico" style="float:right ;padding-left:5px;border:none" src="content/07.png"></a></li>
+                <li><a class="rssfeed hidden-xs hidden-sm navbar-right" href="#"><img class="img-responsive rssico-you" style="float:right ;padding-left:5px;border:none" src="content/12.jpg"></a></li>
+                <li><a class="rssfeed hidden-xs hidden-sm navbar-right" href="#"><img class="img-responsive rssico" style="float:right ;padding-left:5px;border:none" src="content/11.png"></a></li>
+            </ul>
+          <ul class="nav navbar-nav navbar-left">
+            <form class="form-group navbar-right">
+                <div class="input-group">
+                  <input type="search" class="form-control" id="Search" placeholder="Search">
+                  <div class="input-group-addon"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></div>
+                </div>
+            </form>
+            <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+</header>
+<!-- /header -->
 	<div id="content" class="site-content">
--- underscores_them/index.php
+++ underscores_them/index.php
@@ -13,7 +13,8 @@
 
 get_header(); ?>
     
-    <div class="jumbotron">
+    <div class="wrapper">
+        <div class="jumbotron">
             <div class="row">
                 <div class="sisu col-sm-7"> 
                     <h2 class="hidden-sm hidden-xs">Check out these popular tutorials</h2>
@@ -23,35 +24,48 @@ get_header(); ?>
                 </div>
             </div>
         </div>
-    
-	<div id="primary" class="content-area">
+        <div class="main-content">      
+            <div class="row">
+                  <?php if (have_posts()) :
+                        while (have_posts()) : the_post(); ?>
+                  <div class="main-table-items col-sm-6 col-md-4">
+                      <div class="main-table-item-box">
+                          <h3 class="main-table-item-h3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                          <?php the_content(); ?>
+                      </div>
+                  </div>
+                  <?php endwhile;
+                        else :
+                            echo '<p>No content found</p>';
+                        endif; ?>
+	    <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php/* if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php /*while ( have_posts() ) : the_post(); ?>                      
+                            <?php
+                                /* Include the Post-Format-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                /*get_template_part( 'content', get_post_format() );
+                            ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+                    <?php /*endwhile; ?>
 
-			<?php endwhile; ?>
+                    <?php the_posts_navigation(); ?>
 
-			<?php the_posts_navigation(); ?>
+                <?php else : ?>
 
-		<?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                <?php endif; */?>
+       
+		  </main><!-- #main -->
+	   </div><!-- #primary -->
+    </div><!-- #wrapper --> 
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
