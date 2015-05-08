10ab577f59d4f158f2722f7d3e3003d1fa45083e
Margo Junker <margo.junker@khk.ee>
2015-05-08 12:30:38 +0300

    Päeva lõpp 08.05.2015
    08.05.2015 noted:
    
    1) esilehe õpetustest eemalda need oranzid/punased barid õpetuste
    pealt, see et mouse hover teeb tumedakas on piisav.
    
    2) Lisa themele google analytic (võid teha enda konto, aga mulle peab
    ka ligipääs olema pärast lehel)
    
    3) Proovi RSS feed tööle saada.
    
    4) http://178.62.65.175/secret peab viima eraldi tühjale leheküljele,
    mida menüüs ei ole
    
    5) wordpressi perma linkid ei tohiks olla sorteeritud
    /index.php/%year%/%monthnum%/%day%/%postname%/, Proovi panna Post name
    peale, peaks olema mõistlikum.
    
    1) Kaotasin ära punased ja Oranzid read.
    2) Panin peale , aga nüüd syncib
    3) RSS töötab, atom
    4) Muutsin lingi nime, nüüd on olemas (178.62.65.175/secret/)
    5) Muutsin (etc/apache2/apache2.conf <Directory /var/www/> AllowOverride
    All) et permalinke tööle saada.

--- underscores_them/header.php
+++ underscores_them/header.php
@@ -18,7 +18,16 @@
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap-affix.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-62752768-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php wp_enqueue_script("jquery"); ?>
 <?php wp_head(); ?>
 </head>
